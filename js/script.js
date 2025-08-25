3ae6c800d6c05249e0b465c7862565eb214fc157
krishnamoorthy3 <krishnamoorthym3009@gmail.com>
2025-08-25 10:33:33 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function mobileshownav() {
+    $(".nav-toggle-wrapper").addClass("nav-mob-display-n");
+}
+function mobileremovenav() {
+    $(".nav-toggle-wrapper").removeClass("nav-mob-display-n");
+}
